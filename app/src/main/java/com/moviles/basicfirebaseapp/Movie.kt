fdd9b0fecd9d11db38b059e411d556d7408ada4c
mikohel <mikohel96@gmail.com>
2022-06-03 01:00:46 -0500

    RETOFirebase

---
+++ app/src/main/java/com/moviles/basicfirebaseapp/Movie.kt
@@ -0,0 +1,11 @@
+package com.moviles.basicfirebaseapp
+
+class Movie(tittle : String, year : String , id : String , type : String,poster : String , genre: String)  {
+
+    val tittle = tittle
+    val year= year
+    val id = id
+    val type = type
+    val poster= poster
+    val genre = genre
+}
